f6d09df814638a2daf70cbd7e14dafbd2149752f
SiddharthDhumal <siddharthdhumal210@gmail.com>
2025-02-26 11:24:21 +0530

    addded payload limit

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const limiter = rateLimit({
 });
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(helmet());
 app.use("/api", limiter);
 app.use(mongoSanitize());
