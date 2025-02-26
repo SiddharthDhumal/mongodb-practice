315ccc7f1d236b6f430a544456c46b7232ec7a53
SiddharthDhumal <siddharthdhumal210@gmail.com>
2025-02-26 11:08:11 +0530

    added security code

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRouter from "./router.js";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import hpp from "hpp";
+import compression from "compression";
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
@@ -16,10 +23,42 @@ mongoose
 		console.log(`Error is coming while connectiong DB ${error}`);
 	});
 
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: "Too many requests for this Ip, please try again in an hour",
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(userRouter);
+app.use(helmet());
+app.use("/api", limiter);
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+	hpp({
+		whitelist: [
+			"duration",
+			"ratingsQuantity",
+			"ratingsAverage",
+			"maxGroupSize",
+			"difficulty",
+			"price",
+		],
+	})
+);
+
+app.use(compression());
+
+// Test middleware
+app.use((req, res, next) => {
+	req.requestTime = new Date().toISOString();
+	// console.log(req.cookies);
+	next();
+});
+
+app.use("/api", userRouter);
 
 app.listen(PORT, () => {
 	console.log(`server is listening at port ${PORT}`);
--- router.js
+++ router.js
@@ -4,16 +4,16 @@ import { verifyToken } from "./verify.js";
 
 const router = Router();
 
-router.post("/api/register", userController.register);
+router.post("/register", userController.register);
 
-router.post("/api/login", userController.login);
+router.post("/login", userController.login);
 
-router.get("/api/getAllUser", userController.getAllUsers);
+router.get("/getAllUser", userController.getAllUsers);
 
-router.get("/api/user/:id", verifyToken, userController.getUserById);
+router.get("/user/:id", verifyToken, userController.getUserById);
 
-router.put("/api/user/update/:id", verifyToken, userController.updateUser);
+router.put("/user/update/:id", verifyToken, userController.updateUser);
 
-router.delete("/api/deleteUser/:id", verifyToken, userController.deleteUser);
+router.delete("/deleteUser/:id", verifyToken, userController.deleteUser);
 
 export default router;
